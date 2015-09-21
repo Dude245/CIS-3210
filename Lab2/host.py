1b7b5aa24c58c150ebcfe29e7e6480e16a38c8db
Nathan <nreymer@mail.uoguelph.ca>
2015-09-21 15:21:40 -0400

    String returns work

--- Lab2/host.py
+++ Lab2/host.py
@@ -1,18 +1,28 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in todos:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
 class TodoSimple(Resource):
     def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
         return {todo_id: todos[todo_id]}
 
     def put(self, todo_id):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del todos[todo_id]
+        return 'Deleted'
+
 
 api.add_resource(TodoSimple, '/<string:todo_id>')
 
