1607237a07a1d2350a55c71ecdd83d3ddf7263a9
Nathan <reymer_n@hotmail.com>
2015-10-02 17:08:32 -0400

    Dank Memes

--- Lab3/host.py
+++ Lab3/host.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, make_response
+import urllib2
+import json
+import datetime
+import time
+import sys
+import argparse
+
+
 app = Flask(__name__, static_url_path='')
 tasks = {}
-file = open("static/JSON")
-
-# Routes
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
@@ -12,9 +17,18 @@ def root():
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/api/<path:task_id>', methods=['GET','OPTIONS'])
-def get_task(task_id):
-    return jsonify({task_id:tasks[task_id]}),201
+@app.route('/api/nyt/', methods=['GET'])
+def get_task():
+    apiKey="6462dcd33e1d47bc2be98167e19c86ab:10:72958436"
+    dataFromJavascript = request.args.get("data");
+    print dataFromJavascript
+    response = urllib2.urlopen('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+dataFromJavascript+'&api-key='+apiKey)
+    data = json.load(response)
+    #print data['response']['docs'][0]['lead_paragraph']
+    #"http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$('#getInput').val()+"&api-key="+apiKey
+    response.close()
+    return jsonify(data),201
+
 #
 # @app.route('/api/<path:task_id>', methods=['PUT'])
 # def put_task(task_id):
--- Lab3/static/JS/Lab3.js
+++ Lab3/static/JS/Lab3.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
     $("#getButton").click(function(){
-        apiKey="6462dcd33e1d47bc2be98167e19c86ab:10:72958436"
         $('#result').val("");
         $("#getFade").fadeIn();
     });
     $("#getButtonSub").click(function(){
         $.ajax({
             type: "GET",
-            url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$('#getInput').val()+"&api-key="+apiKey,
+            url: "api/nyt/",
+            data: {'data':$('#getInput').val()},
             success: function(data,status,settings) {
-                var title = data.response.snippet;
-                console.log(title);
-            //     $('#result').val(JSON.stringify(data));
-            //     var json = JSON.parse(JSON.stringify(data));
-	        //     alert(json.response.snippet); //mkyong
+                var TheTextBox = document.getElementById("result");
+                TheTextBox.value ="";
+                for (i = 0; i < i<10; i++) {
+                    TheTextBox.value = TheTextBox.value+(JSON.stringify(data['response']['docs'][i]['headline']['main']));
+                    TheTextBox.value = TheTextBox.value+ "\n";
+
+    }
             },
             error:function(data,errorThrown){
-                $('#result').val("Error: "+errorThrown);
+                $('#result').val("An error occured!");
             }
         });
 
