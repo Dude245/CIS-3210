b150ad3bfba0b5c5bc0c38458e46b9117a500c9c
Nathan <reymer_n@hotmail.com>
2015-10-02 23:17:23 -0400

    Works

--- Lab3/host.py
+++ Lab3/host.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 import urllib2
+import urllib
 import json
 import datetime
 import time
@@ -7,8 +8,11 @@ import sys
 import argparse
 
 
+
 app = Flask(__name__, static_url_path='')
 tasks = {}
+jFile = open("static/JSON",'r')
+# Routes
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
@@ -20,14 +24,22 @@ def not_found(error):
 @app.route('/api/nyt/', methods=['GET'])
 def get_task():
     apiKey="6462dcd33e1d47bc2be98167e19c86ab:10:72958436"
-    dataFromJavascript = request.args.get("data");
-    print dataFromJavascript
-    response = urllib2.urlopen('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+dataFromJavascript+'&api-key='+apiKey)
+    keywords = request.args.get("data");
+    response = urllib2.urlopen('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+keywords+'&api-key='+apiKey)
     data = json.load(response)
     #print data['response']['docs'][0]['lead_paragraph']
     #"http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$('#getInput').val()+"&api-key="+apiKey
     response.close()
     return jsonify(data),201
+
+
+@app.route('/api/nyt/static/', methods=['GET'])
+def get_static():
+    line = jFile.readline()
+    data = json.loads(line)
+    return jsonify(data),201
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Lab3/static/JS/Lab3.js
+++ Lab3/static/JS/Lab3.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
         $("#getFade").fadeIn();
     });
     $("#getButtonSub").click(function(){
+         if($('#NYT').is(':checked')) {
         $.ajax({
             type: "GET",
             url: "api/nyt/",
-            data: {'data':$('#getInput').val()},
+            data: {'data':encodeURI($('#getInput').val())},
             success: function(data,status,settings) {
                 var output = document.getElementById("result");
                 output.value ="";
@@ -20,6 +21,26 @@ $(document).ready(function(){
                 $('#result').val("An error occured!");
             }
         });
+    }
+    else {
+        $.ajax({
+            type: "GET",
+            url: "api/nyt/static/",
+            data: {'data':encodeURI($('#getInput').val())},
+            success: function(data) {
+                var output = document.getElementById("result");
+                output.value ="";
+                for (i = 0; i <10; i++) {
+                    output.value = output.value+(JSON.stringify(data['response']['docs'][i]['headline']['main']));
+                    output.value = output.value+ "\n";
+                }
+            },
+            error:function(data,errorThrown){
+                $('#result').val("An error occured!");
+            }
+        });
 
+
+    }
     });
 });
