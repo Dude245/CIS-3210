0fdb8ed3456067db7e869326d9825c32ccfcb2e2
Nathan <reymer_n@hotmail.com>
2015-10-10 02:23:10 -0400

    Here we go again

---
+++ Lab5/static/JS/Lab.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $("#getButton").click(function(){
+        $('#result').val("");
+    });
+    $("#getButtonSub").click(function(){
+        $('.ResultText').empty();
+        if($('#getInput').val()== ""){
+            alert("Error: Cannot submit an empty search")
+        }
+        else {
+        $.ajax({
+            type: "GET",
+            url: "api/nyt/",
+            data: {'data':encodeURI($('#getInput').val())},
+            success: function(data,status,settings) {
+                //console.log(JSON.parse(data))
+                var myObject = JSON.parse(data)
+                console.log(myObject.response.docs.length)
+                for (i = 0; i<myObject.response.docs.length ; i++) {
+                    // console.log(myObject.response.docs[0].headline.main)
+                    // console.log(myObject.response.docs[0].web_url)
+                    line=myObject.response.docs[i].headline.main
+                    link=myObject.response.docs[i].web_url
+                    $('.ResultText').append("<a target=\"_blank\" href="+link+">"+line+"</a><br>");
+                 }
+            },
+            error:function(data,errorThrown){
+            }
+        });
+    }
+    });
+    $("#loadStatic").click(function(){
+        $('.ResultText').empty();
+        $.ajax({
+            type: "GET",
+            url: "api/nyt/static/",
+            data: {'data':''},
+            success: function(data) {
+                var myObject = JSON.parse(data)
+                for (i = 0; i <10; i++) {
+                    line=myObject.response.docs[i].headline.main
+                    link=myObject.response.docs[i].web_url
+                    $('.ResultText').append("<a target=\"_blank\" href="+link+">"+line+"</a><br>");
+                }
+            },
+            error:function(data,errorThrown){
+                $('#result').val("An error occured!");
+            }
+        });
+    });
+});
